2408281d9b1c1178a42d9c978f99ee08c61b0b83
Jonatas Sales <sykes.echo@proton.me>
2026-09-09 16:04:10 -0300

    style: satisfy repository formatter

--- repos/domain/streaming/mockingbird/src/mockingbird/mockingbird.module.ts
+++ repos/domain/streaming/mockingbird/src/mockingbird/mockingbird.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common'
 
 import { TranscodeTrackUseCase } from '@application/use-cases'
-import {
-  StoragePort,
-  TranscoderPort
-} from '@domain/ports'
+import { StoragePort, TranscoderPort } from '@domain/ports'
 import {
   natsConnectionProvider,
   NatsLifecycleService
